826b296092892e407c34e93834f67c0699fca9cf
DaniloBandeira0 <143565487+DaniloBandeira0@users.noreply.github.com>
2025-03-13 17:18:28 -0300

    .

--- clientes/clientes-cadastro.php
+++ clientes/clientes-cadastro.php
@@ -14,7 +14,7 @@
         </div>
         <div class="form-group">
             <label for="telefone">Telefone:</label>
-            <input type="text" class="form-control" name="telefone" id="telefone" placeholder="Digite seu telefone" required>
+            <input type="text" class="form-control" name="telefone" id="telefone" placeholder="Digite seu telefone" required maxlength="11">
             <p id="alertTel"></p>
         </div>
         <div class="form-group">
